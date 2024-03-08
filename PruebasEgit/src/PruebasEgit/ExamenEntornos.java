278d5a8d1ec0478b08c614fa9d9740c9743b640a
Nander75 <159524377+Nander75@users.noreply.github.com>
2024-03-08 11:09:03 +0100

    Intento2

--- PruebasEgit/src/PruebasEgit/ExamenEntornos.java
+++ PruebasEgit/src/PruebasEgit/ExamenEntornos.java
@@ -6,7 +6,7 @@ public class ExamenEntornos {
 		
 		System.out.println("Hola mundo");
 		System.out.println("Hola a tod@s");
-		System.out.println("Cambio desde remoto");
+		System.out.println("Cambio remoto");
 
 	}
 
